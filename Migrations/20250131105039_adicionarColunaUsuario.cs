a4097eed88920fecca455d603bf8704346f01955
Willian Carvalho da Silva <WSILVA@c24h.central24horas>
2025-02-05 17:15:45 -0300

    Adicionei uma coluna usuário ao banco de dados

---
+++ Migrations/20250131105039_adicionarColunaUsuario.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadastroDeComputadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionarColunaUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataDeSaida",
+                table: "Cadastro",
+                newName: "dataDeSaida");
+
+            migrationBuilder.RenameColumn(
+                name: "DataDeEntrada",
+                table: "Cadastro",
+                newName: "dataDeEntrada");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Usuario",
+                table: "Cadastro",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Usuario",
+                table: "Cadastro");
+
+            migrationBuilder.RenameColumn(
+                name: "dataDeSaida",
+                table: "Cadastro",
+                newName: "DataDeSaida");
+
+            migrationBuilder.RenameColumn(
+                name: "dataDeEntrada",
+                table: "Cadastro",
+                newName: "DataDeEntrada");
+        }
+    }
+}
--- models/CadastroModel.cs
+++ models/CadastroModel.cs
@@ -10,19 +10,7 @@ public class CadastroModel {
     public string dataDeEntrada { get; set; } // Inicializado com valor padrão
     public string? dataDeSaida { get; set; } // Tornado anulável
     public TipoEnum Tipo { get; set; }
+    public string Usuario { get; set; }
     public string NFE { get; set; } = string.Empty; // Inicializado para evitar warnings
     public bool Ativo { get; set; } = true; // Inicializado com valor padrão
 }
-
-
-//    public class CadastroModel {
-
-
-//        public SetorEnum Setor { get; set; }
-//        [Key]
-//        public string Tag { get; set; }
-//        public string DataDeEntrada { get; set; }
-//        public string? DataDeSaida { get; set; }
-//        public TipoEnum Tipo { get; set; }
-//        public string NFE { get; set; }
-//        public bool Ativo { get; set; }
